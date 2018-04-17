b6650df0a18d9bd39c461fd712eca51180e3c022
salmangt <salman.ahmed@gaditek.com>
2018-04-17 21:27:16 +0500

    remove zib file

---
+++ DemoFW.framework/Headers/DemoFW.h
@@ -0,0 +1,19 @@
+//
+//  DemoFW.h
+//  DemoFW
+//
+//  Created by Salman  Ahmed on 16/04/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoFW.
+FOUNDATION_EXPORT double DemoFWVersionNumber;
+
+//! Project version string for DemoFW.
+FOUNDATION_EXPORT const unsigned char DemoFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFW/PublicHeader.h>
+
+
---
+++ DemoFW.framework/PrivateHeaders/privateFile.h
@@ -0,0 +1,13 @@
+//
+//  privateFile.h
+//  DemoFW
+//
+//  Created by Salman  Ahmed on 16/04/2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface privateFile : NSObject
+
+@end
